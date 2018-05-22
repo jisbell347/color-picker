04bf6349acd4fdd6fc06d86160b1d96f0bfeb351
Joseph Isbell <shadowwolf347@comcast.net>
2018-05-22 16:53:34 -0600

    Added script js file

---
+++ script.js
@@ -0,0 +1,56 @@
+window.onload = function (){
+	initColorPicker();
+};
+
+function initColorPicker() {
+	let element = document.getElementById("color-box");
+	let colors = {
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
+		blue: document.getElementById("blue")
+	};
+	let colorPickers = document.getElementsByClassName("picker");
+	setColorPickerEventListeners(element, colors, colorPickers);
+}
+
+function setColorPickerEventListeners(element, colors, pickerElements) {
+	let pickerLen = pickerElements.length;
+	for(let i = 0; i < pickerLen; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			console.log("Red value: " + colors.red.value);
+			let red = colors.red.value;
+			let green = colors.green.value;
+			let blue = colors.blue.value;
+			setElementBGColor(element, red, green, blue);
+			setDisplayValues(red, green, blue)
+		});
+	}
+}
+	/*rgb.red.addEventListener('change', () => {
+		console.log("Red Value: ", rgb.red.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+	});
+	rgb.green.addEventListener('change', () => {
+		console.log("Green Value: ", rgb.green.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+	});
+	rgb.blue.addEventListener('change', () => {
+		console.log("Blue Value: ", rgb.blue.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+	});
+}*/
+
+function setElementBGColor(bgElement, red, green, blue) {
+	let rgbVal = [red, green, blue].join(',');
+	bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+	let redVal = document.getElementById("redVal");
+	let greenVal = document.getElementById("greenVal");
+	let blueVal = document.getElementById("blueVal");
+
+	redVal.innerText = red;
+	greenVal.innerText = green;
+	blueVal.innerText = blue;
+}
